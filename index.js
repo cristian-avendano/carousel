2159c32b0a509fb0310123ec8098e0699903c4be
cristian-avendano <cristian-avendano@upc.edu.co>
2020-11-27 17:49:58 -0500

    1.1

--- index.js
+++ index.js
@@ -1,49 +1,27 @@
-let opciones = ["1.jpg", "2.jpg", "3.gif"];
-let cont = document.getElementById("imagen"); 
-let x = 0;
-let izq = document.getElementById("a-izq");
-let der = document.getElementById("a-der");
+const url = ["1.gif", "2.gif", "3.gif"];
+let run = true;
+let count = 1;
+let x =0;
 
-izq.addEventListener('click', ()=>{
-    cizq();
-    change();
-    pos();
-})
-
-der.addEventListener('click', ()=>{
-    cder();
-    change();
-    pos();
-})
+let izq = document.getElementById("izq");
+let der = document.getElementById("der");
+let imagen = document.getElementById("imagen");
+let puntos = document.getElementsByClassName("punto");
 
 
-function pos(){
-    cont.style.backgroundSize="cover";
-    cont.style.backgroundPosition= "center";
-}
-
-function change(){
-    cont.style.background = "url('"+opciones[x]+"')";
-    cont.style.transition = "0.5s";
-}
-
-function fadeIn(el) {
-    el.style.opacity = 0;
-    var tick = function () {
-        el.style.opacity = +el.style.opacity + 0.02;
-        if (+el.style.opacity < 1) {
-            (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
-        }
-    };
-    tick();
-}
-
-function myFunction() {
-    fadeIn(cont);
+function cizq(){
+    run = false;
+    if(x==0){
+        x = url.length-1;
+    }else{
+        x = x-1
+    }
+    console.log(x);
 }
 
 function cder(){
-    if(x==opciones.length-1){
+    run = false;
+    if(x==url.length-1){
         x = 0;
     }else{
         x = x+1
@@ -51,11 +29,19 @@ function cder(){
     console.log(x);
 }
 
-function cizq(){
-    if(x==0){
-        x = opciones.length-1;
-    }else{
-        x = x-1
+function change(){
+    imagen.style.background = "url('"+url[x]+"')";
+    imagen.style.transition = "0.5s";
+    for (let i = 0; i < url.length; i++) {
+        puntos[i].style.background = "none";
     }
-    console.log(x);
+    puntos[x].style.background = "linear-gradient(to right, red, purple)";
 }
+
+function pos(){
+    imagen.style.backgroundSize="cover";
+    imagen.style.backgroundPosition= "center";
+}
+
+izq.addEventListener('click', ()=>{ cizq(); change(); pos();});
+der.addEventListener('click', ()=>{ cder(); change(); pos();});
